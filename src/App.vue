7434438bea961b0a37370048571e78e55d9486e5
carlosbenavides123 <carlos7legend@gmail.com>
2019-01-26 13:42:44 -0800

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,75 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <app-nav-bar></app-nav-bar>
+    <app-banner></app-banner>
+    <app-qualities></app-qualities>
+    <app-contact></app-contact>
+    <app-projects></app-projects>
+    <app-social></app-social>
+    <app-footer></app-footer>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Banner from "./components/Banner.vue";
+import Qualities from "./components/Qualities.vue";
+import Contact from "./components/Contact.vue";
+import Projects from "./components/Projects.vue";
+import Social from "./components/Social.vue";
+import Footer from "./components/Footer.vue";
+import NavBar from "./components/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    appBanner: Banner,
+    appQualities: Qualities,
+    appContact: Contact,
+    appProjects: Projects,
+    appSocial: Social,
+    appFooter: Footer,
+    appNavBar: NavBar
   }
+};
+</script>
+
+<style lang="scss">
+@import "../styles/custom_bootstrap.scss";
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+</style>
+
+<style>
+body {
+  margin-top: 60px;
+}
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-family: "Roboto Condensed", sans-serif;
+}
+.height-100 {
+  min-height: 100vh;
+}
+.height-80 {
+  min-height: 80vh;
+}
+.height-60 {
+  min-height: 60vh;
+}
+.height-50 {
+  min-height: 50vh;
+}
+.height-40 {
+  min-height: 40vh;
+}
+#banner,
+#contact,
+#social {
+  background: linear-gradient(rgba(129, 69, 19, 0.6), rgba(129, 69, 19, 0.6)),
+    url("./assets/img/portret.jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
 }
 </style>
